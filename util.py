f5cee52e37356ca69c54340a4a4914504e1abcf0
onionj <onionj98@gmail.com>
2021-06-18 14:35:07 -0700

    add normal post_data function

--- util.py
+++ util.py
@@ -12,6 +12,25 @@ def make_send_message_api_url(TELEGRAM_TOKEN, ADMIN_CHAT_ID, message) -> str:
     return f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/SendMessage?chat_id={ADMIN_CHAT_ID}&text={message}"
 
 
+def post_data(url, logger) -> bool:
+    try:
+        res = requests.post(url=url, timeout=5)
+        if res.status_code == 200:
+            logger.info('post_data data sended response: 200')
+            return True
+
+        logger.error(
+            f'post_data error: data not sended response: {res.status_code}')
+        return False
+
+    except requests.exceptions.Timeout:
+        logger.error(f'post_data error: timeout ')
+        return False
+    except:
+        logger.error(f'post_data Unknown error')
+        return False
+
+
 def post_data_by_third_party_proxy(url, logger) -> bool:
     '''
     Send information using the http debugger site \n
@@ -31,16 +50,17 @@ def post_data_by_third_party_proxy(url, logger) -> bool:
             timeout=5)
 
         if res.status_code == 200:
-            logger.info('post_data_by_third_party_proxy message sended')
+            logger.info(
+                'post_data_by_third_party_proxy data sended response: 200')
             return True
 
         logger.error(
-            f'post_data_by_third_party_proxy error: message not sended response: {res.status_code}')
+            f'post_data_by_third_party_proxy error: data not sended response: {res.status_code}')
         return False
 
     except requests.exceptions.Timeout:
         logger.error(f'post_data_by_third_party_proxy error: timeout ')
         return False
     except:
-        logger.error(f'post_data_by_third_party_proxy error')
+        logger.error(f'post_data_by_third_party_proxy Unknown error')
         return False
