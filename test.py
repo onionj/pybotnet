fd4b53ab97be83a74297dfb3363842f8de1993a2
onionj <onionj98@gmail.com>
2021-06-18 14:01:54 -0700

    make sample send message method

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 from configs import *
+import pybotnet
+
+bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID, show_log=True)
+
+ans = bot.send_message_by_third_party_proxy('hi admin')
+print(ans)
