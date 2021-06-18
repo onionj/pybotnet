b3053fddcf3c45cd456278b57257948ddff2828a
onionj <onionj98@gmail.com>
2021-06-18 10:03:27 -0700

    add third party proxy server

---
+++ util.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def post_data_by_third_party_proxy(URL):
+    '''
+    Send information using the http debugger site \n
+    Only for special situations: \n
+    Internet restrictions in some countries
+    '''
+    payload = {"UrlBox": URL,
+               "AgentList": "Mozilla Firefox",
+               "VersionsList": "HTTP/1.1",
+               "MethodList": "POST"
+               }
+
+    res = requests.post(
+        "https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx", payload)
+    return res
