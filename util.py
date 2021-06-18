3a577e3bd12c3e30ac7b6c96c1b315ca43cd2e5f
onionj <onionj98@gmail.com>
2021-06-18 12:24:35 -0700

    send message debog

--- util.py
+++ util.py
@@ -4,25 +4,34 @@
 import requests
 
 
-def make_send_message_api_url(TELEGRAM_TOKEN, ADMIN_CHAT_ID, message):
-    return f"https://api.telegram.org/{TELEGRAM_TOKEN}/SendMessage?chat_id={ADMIN_CHAT_ID}&text={message}"
+def make_send_message_api_url(TELEGRAM_TOKEN, ADMIN_CHAT_ID, message) -> str:
+    '''
+    return api url for send message \n
+    return f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/SendMessage?chat_id={ADMIN_CHAT_ID}&text={message}"
+    '''
+    return f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/SendMessage?chat_id={ADMIN_CHAT_ID}&text={message}"
 
 
-def post_data_by_third_party_proxy(URL):
+def post_data_by_third_party_proxy(url) -> bool:
     '''
     Send information using the http debugger site \n
     Only for special situations: \n
     Internet restrictions in some countries
     '''
-    payload = {"UrlBox": URL,
+    payload = {"UrlBox": url,
                "AgentList": "Mozilla Firefox",
                "VersionsList": "HTTP/1.1",
                "MethodList": "POST"
                }
 
     try:
-        return requests.post(
-            "https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx", payload, timeout=5)
+        res = requests.post(
+            url="https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",
+            data=payload,
+            timeout=5)
+        return res.status_code == 200
 
     except requests.exceptions.Timeout:
+        return False
+    except:
         return False
