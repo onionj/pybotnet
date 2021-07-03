2e2e2169310215bfd4291d98053dd001c9279788
onionj <onionj98@gmail.com>
2021-07-04 02:44:14 +0430

    add pypi link

--- pybotnet/settings.py
+++ pybotnet/settings.py
@@ -1,10 +1,16 @@
 
-pybotnet_version = '0.15.5'
+pybotnet_version = '0.15.6'
 pybotnet_github_link = 'https://github.com/onionj/pybotnet'
 pybotnet_licence = 'GNU GENERAL PUBLIC LICENSE ,Version 3, 29 June 2007'
+pybotnet_pypi_link = 'https://pypi.org/project/pybotnet/'
+
 
 pybotnet_info = f"""
-pybotnet version: {pybotnet_version}
-pybotnet github link: {pybotnet_github_link}
-pybotnet licence: {pybotnet_licence}
+            ************ PyBotNet ************
+version:       {pybotnet_version}         
+licence:       {pybotnet_licence}                        
+github link:   {pybotnet_github_link}
+pypi link:     {pybotnet_pypi_link}
+
+            ************           ************
 """
