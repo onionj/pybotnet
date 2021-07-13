3886875fb827f8fc19b8e190aed7f2b3f5e5f59d
onionj <onionj98@gmail.com>
2021-07-13 23:29:34 +0430

    debug

--- pybotnet/settings.py
+++ pybotnet/settings.py
@@ -1,5 +1,5 @@
 
-pybotnet_version = '0.15.9'
+pybotnet_version = '0.16.0'
 pybotnet_github_link = 'https://github.com/onionj/pybotnet'
 pybotnet_licence = 'GNU GENERAL PUBLIC LICENSE ,Version 3, 29 June 2007'
 pybotnet_pypi_link = 'https://pypi.org/project/pybotnet/'
