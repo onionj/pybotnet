a0fafc865a64065303bdd7119065144264dd7b6c
onionj <onionj98@gmail.com>
2021-07-21 00:12:03 +0430

    fix licence

--- pybotnet/settings.py
+++ pybotnet/settings.py
@@ -1,14 +1,14 @@
 
 pybotnet_version = '0.17.0'
 pybotnet_github_link = 'https://github.com/onionj/pybotnet'
-pybotnet_licence = 'GNU GENERAL PUBLIC LICENSE ,Version 3, 29 June 2007'
+pybotnet_license = 'GNU Lesser General Public License v3 or later (LGPLv3+)'
 pybotnet_pypi_link = 'https://pypi.org/project/pybotnet/'
 
 
 pybotnet_info = f"""
             ************ PyBotNet ************
 version:       {pybotnet_version}         
-licence:       {pybotnet_licence}                        
+licence:       {pybotnet_license}                        
 github link:   {pybotnet_github_link}
 pypi link:     {pybotnet_pypi_link}
 
